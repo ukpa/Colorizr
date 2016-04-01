3856f39945e74ad6ae462ad8c6506929ff4701cc
Unnikrishnan Patel <patel.unnikrishnan@gmail.com>
2016-04-01 11:37:17 +0530

    Made changes in color hash

--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -1,21 +1,17 @@
 
 class String
-    @@colors = {:red=>31,:green=>32,:yellow=>33,:blue=>34,:pink=>35,
-    :light_blue=>94,:white=>97,:light_gray=>37,:black=>30}
+    @@colors = {red:31,green:32,yellow:33,blue:34,pink:35,
+    light_blue:94,white:97,light_gray:37,black:30}
 
     def self.colors
-      arr=[]
-      @@colors.each do |key,value|
-        arr << key
-      end
-      arr
+      @@colors.keys
     end
     def self.create_colors
       #code to generate all color methods goes here
       @@colors.each do |color,value|
         self.class_eval("
           def #{color}
-            puts \"\\e[#{value}m\"+self+\"\\e[0m\"
+            puts \"\\e[#{value}m\"+self+\"\\e[39m\"
           end")
       end
     end
